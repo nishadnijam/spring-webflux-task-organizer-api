8f72866127cbdf177dc6678d21f4fbe3b536b7d5
nishadnijam <nishad.nijam@gmail.com>
2023-11-23 21:49:22 +0100

    Initial commit

---
+++ src/main/java/com/github/nishadnijam/springwebfluxtaskorganizerapi/SpringWebfluxTaskOrganizerApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.nishadnijam.springwebfluxtaskorganizerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxTaskOrganizerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxTaskOrganizerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nishadnijam/springwebfluxtaskorganizerapi/SpringWebfluxTaskOrganizerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nishadnijam.springwebfluxtaskorganizerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxTaskOrganizerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
